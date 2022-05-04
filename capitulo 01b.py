40d227889a3a68e020804df7b58624eac2d6b8f9
Danomax <sgtdano@gmail.com>
2022-05-04 13:43:01 -0400

    Update capitulo 01b.py

--- capitulo 01b.py
+++ capitulo 01b.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import tcod
 
-WIDTH, HEIGHT = 720, 480  # Window pixel resolution (when not maximized.)
+WINDOW_WIDTH, WINDOW_HEIGHT = 720, 480  # Window pixel resolution (when not maximized.)
 FLAGS = tcod.context.SDL_WINDOW_RESIZABLE
-
+WIDTH, HEIGHT = 40,30
 
 def main() -> None:
     """Script entry point."""
@@ -13,22 +13,41 @@ def main() -> None:
         "rKRKz.png", 16, 16, tcod.tileset.CHARMAP_CP437,
     )
     
+    player_pos = [int(WIDTH / 2), int(HEIGHT / 2)]
+       
     with tcod.context.new(  # New window with pixel resolution of width×height.
-        width=WIDTH, height=HEIGHT, sdl_window_flags=FLAGS
+        width=WINDOW_WIDTH, height=WINDOW_HEIGHT, sdl_window_flags=FLAGS
     ) as context:
         while True:
-            console = context.new_console(order="F")  # Console size based on window resolution and tile size.
-            console.print(0, 0, "Hello World")
-            context.present(console, integer_scaling=True)
-
+            
+            dx,dy=0,0
+           
+            # Console size based on window resolution and tile size.
+            con = context.new_console(min_columns=5,min_rows=5,order="F")
+            
             for event in tcod.event.wait():
+                if event.type == "KEYDOWN":
+                    if event.sym == tcod.event.K_UP:
+                        (dx,dy) = (0,-1)
+                    if event.sym == tcod.event.K_RIGHT:
+                        (dx,dy) = (1,0)                
+                    if event.sym == tcod.event.K_DOWN:
+                        (dx,dy) = (0,1) 
+                    if event.sym == tcod.event.K_LEFT:
+                        (dx,dy) = (-1,0)  
+                    player_pos[0] += dx
+                    player_pos[1] += dy                     
                 context.convert_event(event)  # Sets tile coordinates for mouse events.
                 print(event)  # Print event names and attributes.
                 if isinstance(event, tcod.event.Quit):
                     raise SystemExit()
                 elif isinstance(event, tcod.event.WindowResized) and event.type == "WINDOWRESIZED":
                     pass  # The next call to context.new_console may return a different size.
+            
+            con.put_char(player_pos[0],player_pos[1],ord('@'))               
+            context.present(con, integer_scaling=True)            
+            
 
-
+            
 if __name__ == "__main__":
     main()
